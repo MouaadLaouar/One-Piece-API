2d94bf11bbfabb449015a4d2587e2a600a256777
Mouaad Mohamed Laouar <74661782+MouaadLaouar@users.noreply.github.com>
2022-06-30 03:25:14 +0100

    Update db.js

--- Config/db.js
+++ Config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }).then((result) => {
         console.log('Connected to MongoDB !!!');
-        charactersModel.find({id : 8 }, (err, result) => {
+        charactersModel.find({}, (err, result) => {
         if(err) {
             console.log(err)
         } else {
