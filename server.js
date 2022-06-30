17eef449197e8af7f24e9749af4caa22c20e28c2
Mouaad Mohamed Laouar <74661782+MouaadLaouar@users.noreply.github.com>
2022-06-30 01:44:08 +0100

    Update server.js
    Fix 7

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.use('/api/quote', quoateRouter);
 app.use('/api/episodes', episodesRouter);
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('./'));
+    app.use(express.static('frontend/dist'));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, './', 'dist', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
     });
 }
 
