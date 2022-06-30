8e2bd793c6e8e6e60b839e64d369e7e7a5272e5b
Mouaad Mohamed Laouar <74661782+MouaadLaouar@users.noreply.github.com>
2022-06-30 03:18:51 +0100

    Update db.js

--- Config/db.js
+++ Config/db.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
+const charactersModel = require('../Model/charactersModel');
+
 
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }).then((result) => {
         console.log('Connected to MongoDB !!!');
+        charactersModel.find({id : 8 }, (err, result) => {
+        if(err) {
+            console.log(err)
+        } else {
+            console.log(result)
+        }
+    })
     })} catch (err) {
         console.log(err);
         process.exit(1);
